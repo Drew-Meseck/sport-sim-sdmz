8ad5e2f40f8e23a26642b0460fe2b9296b6f0a86
Drew-Meseck <42789729+Drew-Meseck@users.noreply.github.com>
2021-06-09 21:37:44 -0600

    Start over with Flask

--- src/mysite/simulation/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import Team, Player, User, Trade, Stat
-
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(User)
-admin.site.register(Trade)
-admin.site.register(Stat)
