41dbd445826adf54539c00368da7f6598474f9a4
Drew-Meseck <42789729+Drew-Meseck@users.noreply.github.com>
2021-06-09 22:00:24 -0600

    added html, css frameworks

--- src/server.py
+++ src/server.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+@app.route("/game")
+def blog():
+    return render_template('game.html')
+
